50479a595947e818464eab6f6c96b633f1ca9182
anyhum97 <anyhum@yandex.ru>
2021-11-13 03:56:51 +0500

    CheckLine & Balance

--- RoverBot/BinanceFutures.cs
+++ RoverBot/BinanceFutures.cs
@@ -24,7 +24,7 @@ namespace RoverBot
 
 		public const string Currency2 = "ETH";
 
-		public const string Version = "0.951";
+		public const string Version = "0.952";
 
 		public static string Symbol = Currency2 + Currency1;
 
@@ -198,7 +198,11 @@ namespace RoverBot
 
 								Logger.Write("OnEntryPointDetected: [Entry Point]");
 
+								Logger.Write(Program.CheckLine);
+
 								PlaceLongOrderAsync(Symbol, volume, price, takeProfit).Wait();
+
+								Logger.Write(Program.CheckLine);
 
 								CheckPositionAsync(Symbol).Wait();
 
@@ -660,6 +664,12 @@ namespace RoverBot
 							UpdateFeePriceAsync().Wait();
 
 							UpdateBalanceAsync().Wait();
+
+							Logger.Write(Program.CheckLine);
+
+							Logger.Write("CheckPosition: Position Closed, Balance = " + Format(Balance, PricePrecision));
+
+							Logger.Write(Program.CheckLine);
 						}
 						
 						IsTrading = await CancelAllOrdersAsync(Symbol);
